72e81443089b4cce6c4ca7322d20cd205638415b
dsjzzwdy0 <dsjzzwdy0@163.com>
2019-01-31 17:53:28 +0800

    create soccer-net.

---
+++ soccer-net/src/main/java/com/loris/net/fetcher/WebFetcher.java
@@ -0,0 +1,40 @@
+/**  
+ * All rights Reserved, Designed By www.loris.com
+ * @Title:  WebFetcher.java   
+ * @Package com.loris.net   
+ * @Description: 本项目用于天津东方足彩数据的存储、共享、处理等   
+ * @author: 东方足彩    
+ * @date:   2019年1月29日 下午5:57:19   
+ * @version V1.0.0
+ * @Copyright: 2019 www.loris.com Inc. All rights reserved. 
+ * 注意：本内容仅限于天津东方足彩有限公司传阅，禁止外泄以及用于其他的商业目
+ */
+package com.loris.net.fetcher;
+
+import com.loris.net.exception.UrlFetchException;
+import com.loris.net.model.WebPage;
+
+/**   
+ * @ClassName:  WebFetcher   
+ * @Description: 数据页面的下载工具类  
+ * @author: 东方足彩
+ * @date:   2019年1月29日 下午5:57:19   
+ *     
+ * @Copyright: 2019 www.tydic.com Inc. All rights reserved. 
+ * 注意：本内容仅限于天津东方足彩有限公司内部传阅，禁止外泄以及用于其他的商业目 
+ */
+public interface WebFetcher
+{
+	/**
+	 * 初始化网页数据下载器
+	 */
+	void init();
+	
+	/**
+	 * 下载数据页面
+	 * @param page 页面数据
+	 * @return 下载是否成功的标志
+	 * @throws UrlFetchException 下载过程中出现的异常
+	 */
+	boolean fetch(WebPage page) throws UrlFetchException;
+}
---
+++ soccer-net/src/main/java/com/loris/net/model/WebPage.java
@@ -0,0 +1,30 @@
+/**  
+ * All rights Reserved, Designed By www.loris.com
+ * @Title:  WebPage.java   
+ * @Package com.loris.net   
+ * @Description: 本项目用于天津东方足彩数据的存储、共享、处理等   
+ * @author: 东方足彩    
+ * @date:   2019年1月29日 下午5:57:51   
+ * @version V1.0.0
+ * @Copyright: 2019 www.loris.com Inc. All rights reserved. 
+ * 注意：本内容仅限于天津东方足彩有限公司传阅，禁止外泄以及用于其他的商业目
+ */
+package com.loris.net.model;
+
+import com.loris.soccer.bean.AutoIdEntity;
+
+/**   
+ * @ClassName:  WebPage   
+ * @Description: 网络数据页面   
+ * @author: 东方足彩
+ * @date:   2019年1月29日 下午5:57:51   
+ *     
+ * @Copyright: 2019 www.tydic.com Inc. All rights reserved. 
+ * 注意：本内容仅限于天津东方足彩有限公司内部传阅，禁止外泄以及用于其他的商业目 
+ */
+public class WebPage extends AutoIdEntity
+{
+	/***/
+	private static final long serialVersionUID = 1L;
+
+}
