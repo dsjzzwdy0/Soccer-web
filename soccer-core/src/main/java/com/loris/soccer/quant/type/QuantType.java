f7998fc2d32719367f14056b1583961521e78cf4
dsjzzwdy0 <dsjzzwdy0@163.com>
2019-05-18 12:06:17 +0800

    create quant

---
+++ soccer-core/src/main/java/com/loris/soccer/quant/type/QuantType.java
@@ -0,0 +1,28 @@
+/**  
+ * All rights Reserved, Designed By www.loris.com
+ * @Title:  @QuantType.java   
+ * @Package com.loris.soccer.quant.typecom.loris.soccer.model   
+ * @Description: 本项目用于天津东方足彩数据的存储、共享、处理等   
+ * @author: 东方足彩    
+ * @date:   2019年1月28日 下午8:59:32   
+ * @version V1.0.0
+ * @Copyright: 2019 www.loris.com Inc. All rights reserved. 
+ * 注意：本内容仅限于天津东方足彩有限公司传阅，禁止外泄以及用于其他的商业目
+ */
+package com.loris.soccer.quant.type;
+
+/**   
+ * @ClassName:  QuantType.java   
+ * @Description: 量化计算的类型  
+ * @author: 东方足彩
+ * @date:   2019年1月28日 下午8:59:32   
+ *     
+ * @Copyright: 2019 www.tydic.com Inc. All rights reserved. 
+ * 注意：本内容仅限于天津东方足彩有限公司内部传阅，禁止外泄以及用于其他的商业目 
+ */
+public enum QuantType
+{
+	PROB, 				//概率
+	PROFIT,				//利润
+	RETAUNRATE			//收益率
+}
