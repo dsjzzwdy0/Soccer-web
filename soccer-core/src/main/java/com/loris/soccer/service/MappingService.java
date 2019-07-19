2363e54809ea79d801f6632e8aae44512e901c5a
dsjzzwdy0 <dsjzzwdy0@163.com>
2019-07-19 16:52:57 +0800

    Add mapping

---
+++ soccer-core/src/main/java/com/loris/soccer/service/MappingService.java
@@ -0,0 +1,27 @@
+/**  
+ * All rights Reserved, Designed By www.loris.com
+ * @Title:  MappingService.java   
+ * @Package com.loris.soccer.service   
+ * @Description: 本项目用于天津东方足彩数据的存储、共享、处理等   
+ * @author: 东方足彩    
+ * @date:   2019年1月28日 下午8:59:32   
+ * @version V1.0.0
+ * @Copyright: 2019 www.loris.com Inc. All rights reserved. 
+ * 注意：本内容仅限于天津东方足彩有限公司传阅，禁止外泄以及用于其他的商业目的
+ */
+package com.loris.soccer.service;
+
+/**   
+ * @ClassName:  MappingService    
+ * @Description: 映射数据服务类：解决中国足彩网、澳客网之间的比赛数据、欧赔、
+ * 			亚盘等数据之间的相互匹配，从页提供全面的数据服务。
+ * @author: 东方足彩
+ * @date:   2019年1月28日 下午8:59:32   
+ *     
+ * @Copyright: 2019 www.loris.com Inc. All rights reserved. 
+ * 注意：本内容仅限于天津东方足彩有限公司内部传阅，禁止外泄以及用于其他的商业目的
+ */
+public interface MappingService
+{
+
+}
